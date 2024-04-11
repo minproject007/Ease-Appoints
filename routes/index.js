56226cfff26426fa9d941083494007075d6e57d9
Sourav Sanjay <souravsanjay07@gmail.com>
2024-04-11 13:15:16 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,84 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("index");
 });
+
+router.get('/index', function(req, res, next) {
+  res.render("index");
+});
+
+router.get('/add-prescription', function(req, res, next) {
+  res.render("add-prescription");
+});
+
+router.get("/appointments", function(req,res){
+  res.render("appointments");
+})
+
+router.get("/booking-success", function(req,res){
+  res.render("booking-success");
+})
+
+router.get("/booking", function(req,res){
+  res.render("booking");
+})
+
+router.get("/checkout", function(req,res){
+  res.render("checkout");
+})
+
+router.get("/doctor-dashboard", function(req,res){
+  res.render("doctor-dashboard");
+})
+
+router.get("/doctor-profile-settings", function(req,res){
+  res.render("doctor-profile-settings");
+})
+
+router.get("/doctor-profile", function(req,res){
+  res.render("doctor-profile");
+})
+
+router.get("/edit-prescription", function(req,res){
+  res.render("edit-prescription");
+})
+
+router.get("/login", function(req,res){
+  res.render("login");
+})
+
+router.get("/my-patients", function(req,res){
+  res.render("my-patients");
+})
+
+router.get("/patient-dashboard", function(req,res){
+  res.render("patient-dashboard");
+})
+
+router.get("/patient-profile", function(req,res){
+  res.render("patient-profile");
+})
+
+router.get("/profile-settings", function(req,res){
+  res.render("profile-settings");
+})
+
+router.get("/register", function(req,res){
+  res.render("register");
+})
+
+router.get("/schedule-timings", function(req,res){
+  res.render("schedule-timings");
+})
+
+router.get("/search", function(req,res){
+  res.render("search");
+})
+
+
+
+
+
 
 module.exports = router;
